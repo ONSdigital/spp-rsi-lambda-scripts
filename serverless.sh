2bac5225f649444eb47dc2cde7e3820d812a5597
Jordancooke <Jordanleecooke@gmail.com>
2021-01-27 13:21:06 +0000

    Removing cd

--- serverless.sh
+++ serverless.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Serverless deployment
-cd spp-rsi-lambda-scripts
 serverless plugin install --name serverless-pseudo-parameters
 serverless plugin install --name serverless-latest-layer-version
 echo Packaging serverless bundle...
--- serverless_remove.sh
+++ serverless_remove.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd spp-rsi-lambda-scripts
 serverless plugin install --name serverless-pseudo-parameters
 serverless plugin install --name serverless-latest-layer-version
 echo Destroying serverless bundle...
