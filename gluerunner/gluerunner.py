cf95f5139ee2c68809de334f624f6c2c033abb5f
Rhodri Guerrier <rhodriguerrier@Rhodris-MacBook-Pro.local>
2021-02-19 10:40:40 +0000

    make glue runner usable by api_handler, ingest glue trigger and emr glue trigger to check and start jobs. The config situation still needs to be solved when grabbing config from arguments of finished ingest glue job

--- gluerunner/gluerunner.py
+++ gluerunner/gluerunner.py
@@ -15,9 +15,13 @@ sfn_worker_name = os.environ.get("sfn_worker_name")
 ddb_table_name = os.environ.get("ddb_table")
 ddb_query_limit = int(os.environ.get("ddb_query_limit"))
 spark_glue_job_capacity = int(os.environ.get("spark_glue_job_capacity"))
+ingest_glue_name = os.environ.get("ingest_glue_name")
+emr_glue_name = os.environ.get("emr_glue_name")
 
 # Set up logger with just environment at first
 logger = general_functions.get_logger(None, current_module, environment, None)
+
+
 glue = boto3.client("glue")
 # Because Step Functions client uses long polling, read timeout has to be > 60 seconds
 sfn_client_config = Config(connect_timeout=50, read_timeout=70)
@@ -25,81 +29,81 @@ sfn = boto3.client("stepfunctions", config=sfn_client_config)
 dynamodb = boto3.resource("dynamodb")
 
 
-def handler(event, context):
+def start_glue_jobs(job_name, config):
     ddb_table = dynamodb.Table(ddb_table_name)
     glue_job_capacity = spark_glue_job_capacity
 
-    # Loop until no tasks are available from Step Functions
-    while True:
+    try:
+        response = glue.start_job_run(
+            JobName=job_name,
+            Arguments=config,
+            MaxCapacity=glue_job_capacity,
+        )
 
-        try:
-            response = sfn.get_activity_task(
-                activityArn=sfn_activity_arn, workerName=sfn_worker_name
-            )
+        glue_job_run_id = response["JobRunId"]
+    except Exception as e:
+        logger.error(f"Error starting glue job {job_name}. Error: {e}")
 
-        except Exception as e:
-            logger.error(f"Failed to get_activity_task for {sfn_activity_arn}: {e} ")
-            return
+    try:
 
-        # Keep the new Task Token for reference later
-        task_token = response.get("taskToken", "")
+        # Store SFN 'Task Token' and Glue Job 'Run Id' in DynamoDB
+        item = {
+            "sfn_activity_arn": sfn_activity_arn,
+            "glue_job_name": job_name,
+            "glue_job_run_id": glue_job_run_id
+        }
 
-        # If there are no tasks, return
-        if not task_token:
-            return
+        ddb_table.put_item(Item=item)
+    except Exception as e:
+        logger.error(f"Error saving glue job in dynamo table {ddb_table_name}. Error: {e}")
 
-        # Parse task input and create Glue job input
-        task_input = ""
-        try:
-            task_input = json.loads(str(response["input"]))
-            if isinstance(task_input, dict):
-                task_input_dict = json.loads(task_input["input"])
-                glue_job_name = task_input["job"]
-            else:
-                task_input_dict = json.loads(task_input)
-                glue_job_name = task_input_dict["GlueJobName"]
 
-            config_string = task_input_dict["--config"]
-            logger_variables = json.loads(config_string)
-            run_id = logger_variables["pipeline"]["run_id"]
-            survey = logger_variables["survey"]
+def check_glue_job(glue_info):
+    args_pass = {}
+    ddb_table = dynamodb.Table(ddb_table_name)
+    glue_job_run_id = glue_info[""]
+    try:
+        ddb_resp = ddb_table.query(
+            KeyConditionExpression=Key("glue_job_run_id").eq(glue_job_run_id)
+        )
+    except dynamodb.exceptions.ResourceNotFoundException as e:
+        logger.error(f"Job {glue_job_run_id} not present in {ddb_table_name}. Error message: {e}")
 
-            logger.info(f"Got task with run id {run_id} survey {survey}")
-        except Exception as e:
-            logger.error(f"Unable to load config: {e}")
-            return
+    if glue_info['detail']['state'] == "SUCCEEDED":
+        logger.info(f"Job with Run Id {glue_job_run_id} SUCCEEDED.")
+        glue = boto3.client("glue")
+        response = glue.get_job_run(JobName=glue_info["detail"]["jobName"], RunId=glue_info["detail"]["jobRunId"])
+        args_pass = response["JobRun"]["Arguments"]
+    elif glue_info['detail']['state'] in ["FAILED", "STOPPED", "TIMEOUT"]:
+        message = f"Glue job {glue_info['detail']['jobName']} run with Run Id {glue_job_run_id[:8]} failed. " \
+                  f"Last state: {glue_info['detail']['state']}. Error message: {glue_info['detail']['message']}"
 
-        try:
-            # We need to base64 encode the json for config otherwise it'll
-            # be mangled on the way through
-            task_input_dict["--config"] = base64.b64encode(
-                task_input_dict["--config"].encode("ascii")
-            ).decode("ascii")
-            logger.info('Running Glue job named "{}"..'.format(glue_job_name))
-            response = glue.start_job_run(
-                JobName=glue_job_name,
-                Arguments=task_input_dict,
-                MaxCapacity=glue_job_capacity,
-            )
+        logger.error(message)
+    else:
+        logger.error(f"Glue job {glue_info['detail']['jobName']} response does not contain state.")
 
-            glue_job_run_id = response["JobRunId"]
+    # After logging and messages then delete item from dynamodb table
+    try:
+        job_key = {
+            "sfn_activity_arn": sfn_activity_arn,
+            "glue_job_run_id": glue_job_run_id
+        }
+        ddb_table.delete_item(Key=job_key)
+    except Exception as e:
+        logger.error(f"Failed to delete glue job, error: {e}")
+    return args_pass
 
-            # Store SFN 'Task Token' and Glue Job 'Run Id' in DynamoDB
-            item = {
-                "sfn_activity_arn": sfn_activity_arn,
-                "glue_job_name": glue_job_name,
-                "glue_job_run_id": glue_job_run_id,
-                "sfn_task_token": task_token,
-            }
 
-            ddb_table.put_item(Item=item)
-            logger.info("Glue job run started. Run Id: {}".format(glue_job_run_id))
+def handler(event, context):
+    if "source" in event and event["source"] == "aws.glue":
+        if event["jobName"] == ingest_glue_name:
+            config_to_pass = check_glue_job(event)
+            start_glue_jobs(emr_glue_name, config_to_pass)
+        elif event["jobName"] == emr_glue_name:
+            config_to_pass = check_glue_job(event)
+    else:
+        # Load config from api handler
+        json_data = event["Records"][0]["body"].replace("\n", " ")
+        event = json.loads(json_data)
 
-        except Exception as e:
-            logger.error(f"Failed to start Glue job named {glue_job_name}: {e}")
-            response = sfn.send_task_failure(
-                taskToken=task_token,
-                error="Failed to start Glue job."
-                      "Check Glue Runner logs for more details.",
-            )
-            return
+        start_glue_jobs(ingest_glue_name, event)
