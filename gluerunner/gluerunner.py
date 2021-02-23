eb7b4b1f6658602e7a0f6e2e6c1523058cf76f3b
Adam Thompson <arthompson1990@gmail.com>
2021-02-23 11:51:22 +0000

    Restyle and fix logging in gluerunner

--- gluerunner/gluerunner.py
+++ gluerunner/gluerunner.py
@@ -22,42 +22,57 @@ def start_glue_jobs(job_name, config):
             Arguments=config,
             MaxCapacity=spark_glue_job_capacity,
         )
-        logger.info(f"Started job {response['JobRunId']}")
-    except Exception as e:
-        logger.error(f"Error starting glue job {job_name}. Error: {e}")
+        logger.info(f"Started job {job_name} with Id {response['JobRunId']}")
+    except Exception:
+        logger.exception(f"Error starting glue job {job_name}")
 
 
 def check_glue_job(glue_info):
     args_pass = {}
 
     if glue_info['detail']['state'] == "SUCCEEDED":
-        logger.info(f"Job with Run Id {glue_info['detail']['jobRunId']} SUCCEEDED.")
+        logger.info(f"Job run {glue_info['detail']['jobRunId']} succeeded")
         response = glue.get_job_run(
             JobName=glue_info["detail"]["jobName"],
             RunId=glue_info["detail"]["jobRunId"]
         )
         args_pass = response["JobRun"]["Arguments"]
+
     elif glue_info['detail']['state'] in ["FAILED", "STOPPED", "TIMEOUT"]:
         logger.error(
-            f"{glue_info['detail']['jobName']} {glue_info['detail']['state']}. Error: {glue_info['detail']['message']}"
+            "Error running %s: run: %s: %s",
+            glue_info['detail']['jobName'],
+            glue_info['detail']['state'],
+            glue_info['detail']['message']
         )
+
     else:
-        logger.error(f"Glue job {glue_info['detail']['jobName']} response does not contain state.")
+        logger.error(
+            "Glue job %s response does not contain state",
+            glue_info['detail']['jobName']
+        )
 
     return args_pass
 
 
 def handler(event, context):
-    if "source" in event and event["source"] == "aws.glue":
+    if event.get("source") == "aws.glue":
         if event["detail"]["jobName"] == ingest_glue_name:
             config_to_pass = check_glue_job(event)
             if config_to_pass:
                 start_glue_jobs(emr_glue_name, config_to_pass)
+
             else:
                 # this means config_to_pass is empty
-                logger.error("Failed to retrieve arguments from finished ingest glue")
+                logger.error(
+                    "Failed to retrieve arguments from finished ingest glue"
+                )
+
         elif event["detail"]["jobName"] == emr_glue_name:
-            config_to_pass = check_glue_job(event)
+            # Check and log the status of the glue job. The only effect here
+            # is a log message.
+            check_glue_job(event)
+
     else:
         # Initial config should be loaded correctly in api_handler.py
         start_glue_jobs(ingest_glue_name, event)
